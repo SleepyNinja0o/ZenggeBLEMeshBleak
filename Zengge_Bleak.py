ee6c58e216f4a3e989e16c66ee9d1dbd8ff19c63
SleepyNinja <hillbilly.tiger@gmail.com>
2023-08-31 17:16:12 -0400

    Create ZenggeColor class

--- Zengge_Bleak.py
+++ Zengge_Bleak.py
@@ -62,70 +62,56 @@ UUID_MANUFACTURER = "00002a29-0000-1000-8000-00805f9b34fb"
 UUID_MODEL = "00002a24-0000-1000-8000-00805f9b34fb"
 UUID_HARDWARE = "00002a27-0000-1000-8000-00805f9b34fb"
 
-
-def convert_value_to_available_range(value, min_from, max_from, min_to, max_to) -> int:
-    normalized = (value - min_from) / (max_from - min_from)
-    new_value = min(
-        round((normalized * (max_to - min_to)) + min_to),
-        max_to,
-    )
-    return max(new_value, min_to)
-
-
-def normal_round(n):
-    if n - math.floor(n) < 0.5:
-        return math.floor(n)
-    return math.ceil(n)
-
-
-def clamp(value, min_value, max_value):
-    return max(min_value, min(max_value, value))
-
-
-def saturate(value):
-    return clamp(value, 0.0, 1.0)
-
-
-def hue_to_rgb(h):
-    r = abs(h * 6.0 - 3.0) - 1.0
-    g = 2.0 - abs(h * 6.0 - 2.0)
-    b = 2.0 - abs(h * 6.0 - 4.0)
-    return saturate(r), saturate(g), saturate(b)
-
-
-def hsl_to_rgb(h, s=1, l=.5):
-    h = (h/360)
-    r, g, b = hue_to_rgb(h)
-    c = (1.0 - abs(2.0 * l - 1.0)) * s
-    r = round((r - 0.5) * c + l,4) * 255
-    g = round((g - 0.5) * c + l,4) * 255
-    b = round((b - 0.5) * c + l,4) * 255
-    if (r >= 250):
-        r = 255
-    if (g >= 250):
-        g = 255
-    if (b >= 250):
-        b = 255
-    return round(r), round(g), round(b)
-
-
-def h360_to_h255(h360):
-    if h360 <= 180:
-        return normal_round((h360*254)/360)
-    else:
-        return normal_round((h360*255)/360)
-
-
-def h255_to_h360(h255):
-    if h255 <= 128:
-        return normal_round((h255*360)/254)
-    else:
-        return normal_round((h255*360)/255)
-
-
-def decode_color(color):
-	red, green, blue = hsl_to_rgb(h255_to_h360(color))
-	return red, green, blue
+class ZenggeColor:
+    def __new__():
+        raise TypeError("This is a static class and cannot be initialized.")
+    @staticmethod
+    def _normal_round(n):
+        if n - math.floor(n) < 0.5:
+            return math.floor(n)
+        return math.ceil(n)
+    @staticmethod
+    def _clamp(value, min_value, max_value):
+        return max(min_value, min(max_value, value))
+    @staticmethod
+    def _saturate(value):
+        return ZenggeColor._clamp(value, 0.0, 1.0)
+    @staticmethod
+    def _hue_to_rgb(h):
+        r = abs(h * 6.0 - 3.0) - 1.0
+        g = 2.0 - abs(h * 6.0 - 2.0)
+        b = 2.0 - abs(h * 6.0 - 4.0)
+        return ZenggeColor._saturate(r), ZenggeColor._saturate(g), ZenggeColor._saturate(b)
+    @staticmethod
+    def _hsl_to_rgb(h, s=1, l=.5):
+        h = (h/360)
+        r, g, b = ZenggeColor._hue_to_rgb(h)
+        c = (1.0 - abs(2.0 * l - 1.0)) * s
+        r = round((r - 0.5) * c + l,4) * 255
+        g = round((g - 0.5) * c + l,4) * 255
+        b = round((b - 0.5) * c + l,4) * 255
+        if (r >= 250):
+            r = 255
+        if (g >= 250):
+            g = 255
+        if (b >= 250):
+            b = 255
+        return round(r), round(g), round(b)
+    @staticmethod
+    def _h360_to_h255(h360):
+        if h360 <= 180:
+            return ZenggeColor._normal_round((h360*254)/360)
+        else:
+            return ZenggeColor._normal_round((h360*255)/360)
+    @staticmethod
+    def _h255_to_h360(h255):
+        if h255 <= 128:
+            return ZenggeColor._normal_round((h255*360)/254)
+        else:
+            return ZenggeColor._normal_round((h255*360)/255)
+    @staticmethod
+    def decode(color):
+        return ZenggeColor._hsl_to_rgb(ZenggeColor._h255_to_h360(color))
 
 
 class ZenggeCloud:
@@ -307,7 +293,7 @@ class ZenggeMesh:
             white_temperature = color = struct.unpack('B', data[14:15])[0] #should be [14:15][0]
             if(mode == 63 or mode == 42):
                 color_mode = 'rgb'
-                rgb = decode_color(color) #Converts from 1 value(kelvin) to RGB
+                rgb = ZenggeColor.decode_color(color) #Converts from 1 value(kelvin) to RGB
             else:
                 color_mode = 'white'
                 rgb = [0,0,0]
